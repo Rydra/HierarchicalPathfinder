239bc6613aa2e4625039fa3c8c4cc145f586ec55
Rydra <davigetto@gmail.com>
2016-02-13 13:49:37 +0100

    Added forgotten file

---
+++ HPASharp/IPassability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPASharp
+{
+	public interface IPassability
+	{
+		/// <summary>
+		/// Tells whether for a given position this passability class can enter or not.
+		/// </summary>
+		bool CanEnter(Position pos, out int movementCost);
+	}
+}
